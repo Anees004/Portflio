f586d10008b75c6d41ffc2a32b63b6c32964dc98
Anees004 <aneesahmad00423@gmail.com>
2025-02-24 03:24:57 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Anees Just want to publish portfolio
         </a>
       </header>
     </div>
